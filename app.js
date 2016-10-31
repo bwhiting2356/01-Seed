c891502f6dfade652f869091fdedb4cd5382dee9
Brendan Whiting <brendan.whiting@me.com>
2016-10-31 01:24:50 -0400

    first commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var messageRoutes = require('./routes/messages');
 var userRoutes = require('./routes/user');
 
 var app = express();
-mongoose.connect('localhost:27017/node-angular');
+mongoose.connect('test-user:test_pw@ds139277.mlab.com:39277/angular2-deployment');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -4,40 +4,27 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from "./app.component";
-import { MessageComponent } from './messages/message.component';
-import { MessageListComponent } from './messages/message-list.component';
-import { MessageInputComponent } from './messages/message-input.component';
-import { MessagesComponent } from './messages/messages.component';
 import { ErrorComponent }  from './errors/error.component';
 import { ErrorService } from './errors/error.service';
 import { AuthenticationComponent } from './auth/authentication.component';
 import { HeaderComponent } from './header.component';
-import { LogoutComponent } from './auth/logout.component';
-import { SigninComponent } from './auth/signin.component';
-import { SignupComponent } from './auth/signup.component';
 import { AuthService } from './auth/auth.service';
 import { routing } from './app.routing';
+import { MessageModule } from './messages/message.module';
 
 @NgModule({
     declarations: [
         AppComponent, 
-        MessageComponent,
-        MessageListComponent,
-        MessageInputComponent,
-        MessagesComponent,
         AuthenticationComponent,
         HeaderComponent,
-        LogoutComponent,
-        SigninComponent,
-        SignupComponent,
         ErrorComponent
     ],
     imports: [
         BrowserModule, 
         FormsModule, 
-        ReactiveFormsModule, 
         routing,
-        HttpModule],
+        HttpModule,
+        MessageModule],
     providers: [AuthService, ErrorService],
     bootstrap: [AppComponent]
 })
